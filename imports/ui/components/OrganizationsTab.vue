8d15443b7d09750ac6dfee0af99050389adb1b43
Rodan Ramdam <rodan.ramdam@gmail.com>
2022-11-09 20:11:52 +0545

    feat: setup organization management methods, option to add new organization, add list of organizations in organization tab

--- imports/ui/components/OrganizationsTab.vue
+++ imports/ui/components/OrganizationsTab.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>Organizations Tab</h1>
+  <div class="heading">
+    <div class="tab-title">
+      <h1>Organizations</h1>
+    </div>
+    <div class="content-body">
+      <AddOrganizationForm />
+      <ul class="organizations-list">
+        <li v-for="organization in organizations" v-bind:key="organization._id">
+          {{ organization.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
+import AddOrganizationForm from "./AddOrganizationForm.vue";
+import { OrganizationsCollection } from "../../db/OrganizationsCollection";
 export default {
   name: "OrganizationsTab",
-  data() {},
+  components: {
+    AddOrganizationForm,
+  },
+  meteor: {
+    $subscribe: {
+      organizations: [],
+    },
+    organizations() {
+      return OrganizationsCollection.find(
+        {},
+        { sort: { createdAt: -1 } }
+      ).fetch();
+    },
+  },
 };
 </script>
+<style scoped></style>
--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 import { Meteor } from "meteor/meteor";
-//import methods here
+import "/imports/api/organizationsMethods";
+import "/imports/api/organizationsPublications";
+import { OrganizationsCollection } from "/imports/db/OrganizationsCollection";
 
 Meteor.startup(() => {
   //code when the server starts
+  OrganizationsCollection.createIndex({ name: 1 }, { unique: true }); // making name of organization unique
 });
